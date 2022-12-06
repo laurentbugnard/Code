f2a979243a465ca44d8e0dddb01d45bf8f533f0f
laurentbugnard <laurent.bugnard@epfl.ch>
2022-12-06 20:36:36 +0100

    Started visualization

--- evolution.py
+++ evolution.py
@@ -1,20 +1,66 @@
 import numpy as np
 
 
-def evolution(system, nstep, max_relaxation_steps = 100000):
-    sigma = np.empty([nstep + 1])  # average stress
-    epsp = np.empty([nstep + 1])  # average plastic strain
-    sigma[0] = system.sigmabar
-    epsp[0] = np.mean(system.epsp)
+def evolution(system, nstep: int):
+
+    sigmabar = np.empty([nstep + 1])  # average stress
+    epspbar = np.empty([nstep + 1])  # average plastic strain
+    sigmabar[0] = system.sigmabar
+    epspbar[0] = np.mean(system.epsp)
 
     for i in range(1, nstep+1):
-        system.eventDrivenStep(max_steps = max_relaxation_steps)
-        sigma[i] = system.sigmabar
-        epsp[i] = np.mean(system.epsp)
+        system.shiftImposedShear()
+        system.relaxAthermal()
+        sigmabar[i] = system.sigmabar
+        epspbar[i] = np.mean(system.epsp)
 
-    if(np.sum((np.diff(epsp) < 0)) > 0):
+    if(np.sum((np.diff(epspbar) < 0)) > 0):
         print('Warning: epsp not monotonic!')
-    return sigma,epsp
+    return sigmabar,epspbar
+
+def evolution_verbose(system, nstep: int):
+    """Evolves the system by ``nstep`` and returns means and maps of ``sigma`` and ``epsp`` for each step. Additionally, returns number of relaxation steps and indexes of first failing block for each avalanche.
+
+    Args:
+        system (SystemAthermal): The system to evolve.
+        nstep (int): The number of steps.
+
+    Returns:
+        sigmabar: List of mean stress.
+        epspbar: List of mean plastic strain.
+        sigma: List of stress maps.
+        epsp: List of plastic strain maps.
+        relax_steps: Number of relaxation steps for each avalanche.
+        failing: List of failing indexes.
+    """
+    sigmabar = np.empty([nstep + 1])  # average stress
+    sigmabar[0] = system.sigmabar
+    
+    epspbar = np.empty([nstep + 1])  # average plastic strain
+    epspbar[0] = np.mean(system.epsp)
+    
+    sigma = [system.sigma]
+    
+    epsp = [system.epsp]
+    
+    relax_steps = np.empty([nstep + 1])
+    relax_steps[0] = 0
+    
+    failing = np.empty([nstep])
+    
+
+    for i in range(1, nstep+1):
+        system.shiftImposedShear()
+        relax_steps[i] = system.relaxAthermal()
+        
+        sigmabar[i] = system.sigmabar
+        epspbar[i] = np.mean(system.epsp)
+        sigma.append(system.sigma)
+        epsp.append(system.epsp)
+        failing[i-1] = np.argmax(np.abs(system.sigma) - system.sigmay) #TODO do it later instead (after evolution, before plotting --> when computing more things). Here, do only what is strictly necessary to extract. But then we need to extract sigmay at each step (or at least its changes).
+        
+    return sigmabar, epspbar, sigma, epsp, relax_steps, failing
+        
 
 def find_runtime_error(system, nstep, max_relaxation_steps = 100000):
 
--- examples/viz.py
+++ examples/viz.py
@@ -3,12 +3,21 @@ import numpy as np
 from GooseEPM import elshelby_propagator
 from GooseEPM import SystemAthermal
 import matplotlib.pyplot as plt
+from evolution import *
+plt.style.use('./config/style.mplstyle')
 
 #%%
 L = 50
+#TODO ask to have direct access to sigmay_mean, propagator, etc.
+
+sigmay_mean=np.ones([L, L])
+propagator, distances_rows, distances_cols = elshelby_propagator(L=L, imposed="strain")
+
 system = SystemAthermal(
-    *elshelby_propagator(L=L, imposed="strain"),
-    sigmay_mean=np.ones([L, L]),
+    propagator=propagator,
+    distances_rows=distances_rows,
+    distances_cols=distances_cols,
+    sigmay_mean=sigmay_mean,
     sigmay_std=0.3 * np.ones([L, L]),
     seed=0,
     init_random_stress=False,
@@ -17,35 +26,100 @@ system = SystemAthermal(
 )
 
 #%%
-nstep = 1000
-sigma = np.empty([nstep])  # average stress
-epsp = np.empty([nstep])  # average plastic strain
-sigma[0] = system.sigmabar
-epsp[0] = np.mean(system.epsp)
-
-for i in range(1, nstep):
-    system.shiftImposedShear()
-    system.relaxAthermal()
-    sigma[i] = system.sigmabar
-    epsp[i] = np.mean(system.epsp)
+sigmabar, epspbar, sigma, epsp, relax_steps, failing = evolution_verbose(system, 1000)
 
 
-#%%
+#%% Image box
 
-fig, axes = plt.subplots(ncols=2, figsize=(8 * 2, 6))
+fig, axes = plt.subplots(ncols=2)
 
 ax = axes[0]
-ax.plot([0, 0.6], [0, 0.6], "r--")
-ax.plot(sigma + epsp, sigma)
-ax.set_xlabel(r"$\gamma$")
+cax = ax.imshow(sigma[-1])
+ax.set_title(r'$\sigma(x)$')
+# cbar = fig.colorbar(cax)
+
+ax = axes[1]
+ax.imshow(epsp[-1])
+ax.set_title(r'$\epsilon_p(x)$')
+
+plt.show()
+#%% Evolution box
+
+fig, axes = plt.subplots(ncols=2)
+fig.tight_layout
+
+ax = axes[0]
+ax.plot(sigmabar + epspbar, sigmabar)
+ax.set_xlabel(r"$\epsilon$")
 ax.set_ylabel(r"$\sigma$")
 
 ax = axes[1]
-cax = ax.imshow(system.epsp, interpolation="nearest")
-
-cbar = fig.colorbar(cax, aspect=10)
-cbar.set_label(r"$\gamma_p$")
+ax.plot(relax_steps)
+ax.set_xlabel(r"step")
+ax.set_ylabel(r"$relaxation steps$")
 
 plt.show()
+
+#%% Parameter box
+
+fig, axes = plt.subplots(1,2)
+fig.suptitle(f'L = {L}')
+
+ax = axes[0]
+ax.imshow(sigmay_mean)
+ax.set_title(r'$\sigma(x)$')
+
+ax = axes[1]
+ax.imshow(propagator)
+ax.set_title(r'$\epsilon_p(x)$')
+
+
+# %% Avalanches box
+
+
+#%% All together
+fig = plt.figure()
+subfigs = fig.subfigures(2,2,wspace=0.3, width_ratios=[2,1])
+
+#Images
+axes_images = subfigs[0,0].subplots(1,2)
+
+ax = axes_images[0]
+ax.imshow(sigma[-1])
+ax.set_title(r'$\sigma(x)$')
+
+ax = axes_images[1]
+ax.imshow(epsp[-1])
+ax.set_title(r'$\epsilon_p(x)$')
+
+#Parameters
+axes_parameters = subfigs[0,1].subplots(1,2)
+
+ax = axes_parameters[0]
+ax.imshow(sigmay_mean)
+ax.set_title(r'$\sigma(x)$')
+
+ax = axes_parameters[1]
+ax.imshow(propagator)
+ax.set_title(r'$\epsilon_p(x)$')
+
+#Plots
+axes_plots = subfigs[1,0].subplots(1,2)
+
+ax = axes_plots[0]
+ax.plot(sigmabar + epspbar, sigmabar)
+ax.set_xlabel(r"$\epsilon$")
+ax.set_ylabel(r"$\sigma$")
+
+ax = axes_plots[1]
+ax.plot(relax_steps)
+ax.set_xlabel(r"step")
+ax.set_ylabel(r"$relaxation steps$")
+
+#Avalanches
+axes_avalanches = subfigs[1,1].subplots(1,2)
+
+figManager = plt.get_current_fig_manager()
+figManager.window.showMaximized()
 
 # %%
